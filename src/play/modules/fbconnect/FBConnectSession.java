4493ef06713fff502927cfbe3c9f776fa1087685
Michael Murray <mmurray.wa@gmail.com>
2011-02-06 18:04:21 -0800

    taking out the authCode string replace for now

--- src/play/modules/fbconnect/FBConnectSession.java
+++ src/play/modules/fbconnect/FBConnectSession.java
@@ -44,8 +44,7 @@ public class FBConnectSession {
     public String getAuthUrl(String authCode){
         return "https://graph.facebook.com/oauth/access_token?client_id=" +
         WS.encode(id)+"&redirect_uri=" +
-        WS.encode(Router.getFullUrl("FBConnect.callback")) + "&client_secret="+WS.encode(secret)+"&code="+WS.encode(authCode.replace("|", "%7C"));
-
+        WS.encode(Router.getFullUrl("FBConnect.callback")) + "&client_secret="+WS.encode(secret)+"&code="+WS.encode(authCode);
     }
     
     public void init(){
